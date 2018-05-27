143815cf9f7c3aa2bde50176e4d796af1816e652
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-27 11:38:33 +0200

    script transition images

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var canvas=["gamejam.svg","gamejam2.svg","gamejam3.svg","gamejam4.svg","gamejam5
 var i=0
 $('#canvass').attr('src','assets/'+canvas[i])
 $('#canvas').click(function(){
-	$('#canvass').attr('src','assets/'+canvas[i])
-	i++
+	$('#canvass').fadeOut('medium');
+	i++;
+	$('#canvass').fadeIn('medium')
+	setTimeout(affiche,400)
 })
+function affiche(){
+	$('#canvass').attr('src','assets/'+canvas[i]);
+}
