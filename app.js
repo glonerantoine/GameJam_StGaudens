e7d00714375c1bfc903b039dab8ce6662d74b80f
glonerantoine <antoine.gloner@orange.fr>
2018-05-27 13:46:06 +0200

    modification du JS et du fichierbd.html

--- app.js
+++ app.js
@@ -6,7 +6,29 @@ $('#canvas').click(function(){
 	i++;
 	$('#canvass').fadeIn('medium')
 	setTimeout(affiche,400)
+
+	choice()
+
+	//changement de text / fadeOut  FadeIn Text 
 })
 function affiche(){
 	$('#canvass').attr('src','assets/'+canvas[i]);
 }
+var scene = ["<span class='nom'>Père</span> : C'est pas possible. Tu prends tes affaires et tu te tires !<br /> <span class='nom'>Mère</span> : Tu ne remets plus jamais les pieds ici ! Tu ne t'approches pas de nous. Tu ne cherches pas à nous revoir. Tu ne nous appelles pas. Tu ne parles pas de nous."];
+var scene2 = ["<p>Bonjour</p>"];
+function choice() {
+		switch(i){
+			case 0:
+			document.getElementById("text").innerHTML = scene;
+			break;
+			case 1:
+			document.getElementById("text").innerHTML = scene2;
+		}
+
+
+
+
+
+}
+
+choice();
