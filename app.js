85fd4bfa9b0ab77bae85e7d82657e68dbe56be3a
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-27 12:48:44 +0200

    changement banque image

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var canvas=["gamejam.svg","gamejam2.svg","gamejam3.svg","gamejam4.svg","gamejam5.svg"];
+var canvas=["gamejam.svg","gamejam2.svg","gamejam3.svg","gamejam4.svg","gamejam5.svg","gamejam6.svg"];
 var i=0
 $('#canvass').attr('src','assets/'+canvas[i])
 $('#canvas').click(function(){
